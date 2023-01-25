dca040adfd360fb771bd129fccc410792638c341
Erik Drobne <erik.drobne@gmail.com>
2023-01-25 10:07:11 +0100

    Fix indentations in TouchTransparentView.

--- SwiftUIInspector/Sources/SwiftUIViewInspector/TouchTransparentView.swift
+++ SwiftUIInspector/Sources/SwiftUIViewInspector/TouchTransparentView.swift
@@ -12,14 +12,15 @@ import AppKit
 #endif
 
 final class TouchTransparentView: PlatformView {
-  var pointInsideCallback: (PlatformPoint, PlatformView) -> Void = { _, _ in }
-#if os(iOS)
-  override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
-    pointInsideCallback(point, self)
-    return false
-  }
-#else
-  #warning("input handling on macOS not implemented yet")
-#endif
+    var pointInsideCallback: (PlatformPoint, PlatformView) -> Void = { _, _ in }
+
+    #if os(iOS)
+        override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+            pointInsideCallback(point, self)
+            return false
+        }
+    #else
+        #warning("input handling on macOS not implemented yet")
+    #endif
 }
 
