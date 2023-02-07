7086f26b4bdd4d6030773db9503bbefb84bb0959
Vid Tadel <tadelv@gmail.com>
2023-02-07 10:36:09 +0100

    initial implementation of overlay

--- SwiftUIInspector/Sources/SwiftUIViewInspector/InspectionViewCoordinator.swift
+++ SwiftUIInspector/Sources/SwiftUIViewInspector/InspectionViewCoordinator.swift
@@ -5,21 +5,31 @@
 //  Created by Vid Tadel on 1/24/23.
 //
 import Foundation
+#if os(iOS)
+import UIKit
+#endif
 
 extension InspectionView {
 	class Coordinator: NSObject {
 		var candidate: PlatformView? {
 			willSet {
 				borderView?.removeFromSuperview()
+#if os(iOS)
+        candidateInfoView?.removeFromSuperview()
+#endif
 			}
 			didSet {
 				guard let candidate else {
 					return
 				}
 				candidate.addSubview(borderView(candidate.frame.size))
+        displayViewInfo(candidate)
 			}
 		}
 		var borderView: PlatformView?
+#if os(iOS)
+    var candidateInfoView: UILabel?
+#endif
 
 		override init() {
 
@@ -59,6 +69,41 @@ extension InspectionView {
 			borderView = view
 			return view
 		}
+
+    func displayViewInfo(_ view: PlatformView) {
+#if os(iOS)
+      candidateInfoView?.removeFromSuperview()
+      candidateInfoView = nil
+      guard let window = view.window else {
+        return
+      }
+
+      candidateInfoView = UILabel()
+      guard let candidateInfoView else { return }
+      candidateInfoView.translatesAutoresizingMaskIntoConstraints = false
+      candidateInfoView.text = view.frame.displayFriendly
+      candidateInfoView.font = .monospacedDigitSystemFont(ofSize: 10, weight: .medium)
+      candidateInfoView.backgroundColor = .blue
+      candidateInfoView.textColor = .white
+      candidateInfoView.layer.compositingFilter = "differenceBlendMode"
+      candidateInfoView.sizeToFit()
+      window.addSubview(candidateInfoView)
+      window.bringSubviewToFront(candidateInfoView)
+      NSLayoutConstraint.activate([
+        candidateInfoView.topAnchor.constraint(equalTo: window.safeAreaLayoutGuide.topAnchor),
+        candidateInfoView.leadingAnchor.constraint(equalTo: window.safeAreaLayoutGuide.leadingAnchor),
+      ])
+#endif
+    }
 	}
 }
 
+#if os(iOS)
+extension CGRect {
+  var displayFriendly: String {
+    """
+    x: \(round(origin.x * 100) / 100), y: \(round(origin.y * 100) / 100), \(round(size.width * 100) / 100)x\(round(size.height * 100) / 100)
+    """
+  }
+}
+#endif
