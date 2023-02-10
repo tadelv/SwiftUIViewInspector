a1cc019b8ee59087ed186ce2daf0dddb6debc95a
Erik Drobne <erik.drobne@gmail.com>
2023-02-10 09:18:32 +0100

    Update project settings.

--- SwiftUIInspector/Package.swift
+++ SwiftUIInspector/Package.swift
@@ -7,23 +7,15 @@ let package = Package(
     name: "SwiftUIViewInspector",
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUIViewInspector",
             targets: ["SwiftUIViewInspector"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUIViewInspector",
-            dependencies: []),
-//        .testTarget(
-//            name: "SwiftUIViewInspectorTests",
-//            dependencies: ["SwiftUIViewInspector"])
+            dependencies: [],
+            path: "Sources"
+        )
     ]
 )
